aae60d5b0b628306296b031a872ecbe3d293aedd
Shraman Karmakar <karmakarshraman08@gmail.com>
2025-07-29 21:24:22 +0530

    Add files via upload

---
+++ Backend_3/script.js
@@ -0,0 +1,5 @@
+// node js -> Module
+// npm -> Packages
+// installation -> npm install (package name)....for versions write @req.version after package name
+// dependencies -> packages and packages ki dependencies
+// devdependencies -> aisa packages jo sirf development mein kaam aaye par jab app ban jayega aur upload ho jayega tab packages ko use nahi kar rahe honge
