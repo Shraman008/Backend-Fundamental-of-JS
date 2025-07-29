5afcb56123a7475014f31da54b983cb492db1042
Shraman Karmakar <karmakarshraman08@gmail.com>
2025-07-29 21:45:07 +0530

    Add files via upload

---
+++ Backend_6/views/Untitled-1.cs
@@ -0,0 +1,21 @@
+/*
+setting up parsers for form
+
+setting up ejs for ejs pages
+    install ejs from npm
+    setup ejs as view engine 
+
+setting up public static files
+
+dynamic routing
+    dynamic routing :-
+    (sabse pahele browser par jao
+    url likhiye apna jo apko chahiye and enter dabaiye
+    ab us url route ko create kariye
+    res bhejiye kuch bhi
+    ab usi url ko dynamic banana hai to realise karo konsa part dynamic ha aur us part ke age route mein colon laga do )
+
+    how to get data coming from frontend at backend route
+
+<%= 2+2 %> for calculation of any value
+*/
